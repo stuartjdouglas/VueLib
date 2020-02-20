0862bea782be244e24f4c4be4c0c5ec55ea84a3f
Stuart Douglas <stuartjdouglas@icloud.com>
2020-02-20 21:06:29 +0000

    Initial library and demo app with tutorial incrementing button

---
+++ demo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+        <incrementing-button />
+
+  </div>
+</template>
+
+<script>
+import IncrementingButton from 'lib';
+
+export default {
+  name: 'App',
+  components: {
+    IncrementingButton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lib/src/components/IncrementingButton.vue
@@ -0,0 +1,31 @@
+/**
+ * Incrementing button as part of tutorial.
+ */
+<template>
+  <div>
+    <button @click="increment">{{text}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  computed: {
+    times() {
+      return this.count > 1 ? "times" : "time";
+    },
+    text() {
+      return `I have been clicked ${this.count} ${this.times}`;
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    }
+  }
+};
+</script>
